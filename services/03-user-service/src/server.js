7cc9b1370a4882deb114738826b8a2d2d7503ac6
NguyenIsHere <22520988@gm.uit.edu.vn>
2025-05-26 11:39:48 +0700

    direct ip answer

--- services/03-user-service/src/server.js
+++ services/03-user-service/src/server.js
@@ -86,8 +86,8 @@ async function main () {
       )
 
       const consul = new Consul({ host: CONSUL_AGENT_HOST, promisify: true })
-      // const serviceAddressForConsul = SERVICE_NAME
-      const serviceAddressForConsul = '172.23.0.4'
+      const serviceAddressForConsul = SERVICE_NAME
+      // const serviceAddressForConsul = '172.23.0.4'
 
       const instanceId =
         process.env.HOSTNAME || crypto.randomBytes(8).toString('hex')
